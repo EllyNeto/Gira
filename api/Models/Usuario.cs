9cd016456e2e6794933dac8e8970ac9e56bd5e94
Celsadasotimas <armindocesar81@gmail.com>
2024-06-21 20:05:05 +0100

    DEU CERTO

---
+++ api/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api.Models;
+
+public partial class Usuario
+{
+    public int id { get; set; }
+
+    public string? nome { get; set; }
+
+    public DateOnly data_de_nascimento {get; set; }
+
+    public string? telefone { get; set; }
+
+    public string email { get; set; } = null!;
+
+    public string senha { get; set; } = null!;
+
+    public short nivel_acesso { get; set; }
+
+    public virtual ICollection<InfoView> InfoViews { get; set; } = new List<InfoView>();
+
+    public virtual ICollection<Informacoes> Informacoes { get; set; } = new List<Informacoes>();
+}
---
+++ api/Program.cs
@@ -0,0 +1,68 @@
+using api.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found"));
+
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GiraCom", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GiraCom V1");
+    c.RoutePrefix = string.Empty; // Define a página inicial para o Swagger
+});
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers(); // Adicione isto para registrar os controladores
+app.Run();
