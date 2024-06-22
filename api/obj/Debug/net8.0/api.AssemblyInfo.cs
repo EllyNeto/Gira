c3cc6a0d693bd58da8b5b19629f586449d9396f5
Celsadasotimas <armindocesar81@gmail.com>
2024-06-23 00:32:46 +0100

    wowowh

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e736d31e92dfcbabaca25e57003512206c19402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd016456e2e6794933dac8e8970ac9e56bd5e94")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
